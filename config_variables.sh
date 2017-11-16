927203454eca5aa675fd161c21364adeb2449557
Waldo Jaquith <waldo@jaquith.org>
2017-11-16 09:05:20 -0500

    Test config file in Travis CI
    Toward #5.

--- config_variables.sh
+++ config_variables.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-variables=(LIS_FTP_USERNAME
-	LIS_FTP_PASSWORD)
-#	PDO_DSN
-#	PDO_SERVER
-#	PDO_USERNAME
-#	PDO_PASSWORD
-#	GMAPS_KEY
-#	YAHOO_KEY
-#	OPENSTATES_KEY
-#	OPENVA_KEY
-#	VA_DECODED_KEY
-#	MAPBOX_TOKEN
-#   MEMCACHED_SERVER
-#	PUSHOVER_KEY
-#	SLACK_WEBHOOK
-#)
+variables=(
+	LIS_FTP_USERNAME
+	LIS_FTP_PASSWORD
+	PDO_DSN
+	PDO_SERVER
+	PDO_USERNAME
+	PDO_PASSWORD
+	GMAPS_KEY
+	YAHOO_KEY
+	OPENSTATES_KEY
+	OPENVA_KEY
+	VA_DECODED_KEY
+	MAPBOX_TOKEN
+	MEMCACHED_SERVER
+	PUSHOVER_KEY
+	SLACK_WEBHOOK
+)
 
 # Iterate over the variables and make sure that they're all populated.
-echo Password: $LIS_FTP_PASSWORD
 for i in "${variables[@]}"
 do
-	if [ -z "${!i}" ]
-		echo "$i not set -- aborting"
-		then exit 1
+	if [ -z "${!i}" ]; then
+		echo "Travis CI has no value set for $i -- aborting"
 	fi
 done
 
 # Now iterate over again and perform the replacement.
 for i in "${variables[@]}"
 do
-	sed -i -e 's/define(\'$i\', \'\')/define(\'${!i}\', \'\')/g' includes/settings.inc.php
+	sed -i -e 's/define('\''$i'\'', '\'''\'')/define('\''$i'\'', '\''${!i}'\'')/g' includes/settings-test.inc.php
 done
+
+echo includes/settings-test.inc.php
