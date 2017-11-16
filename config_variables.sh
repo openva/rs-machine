eb5564227e09c029038a99560bd623febcf89af3
Waldo Jaquith <waldo@jaquith.org>
2017-11-16 09:33:30 -0500

    Fix settings file name, to be displayed
    Toward #5

--- config_variables.sh
+++ config_variables.sh
@@ -32,4 +32,4 @@ do
 	sed -i -e 's/define('\''$i'\'', '\'''\'')/define('\''$i'\'', '\''${!i}'\'')/g' includes/settings.inc.php
 done
 
-cat includes/settings-test.inc.php
+cat includes/settings.inc.php
