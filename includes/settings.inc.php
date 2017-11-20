720b436714a46ed43e7de3b3b5fbc0a11b332474
Waldo Jaquith <waldo@jaquith.org>
2017-11-20 08:04:15 -0500

    Flip the switch for the 2018 session

--- includes/settings.inc.php
+++ includes/settings.inc.php
@@ -10,16 +10,16 @@
 
 # THE CURRENT SESSION
 # As defined by Richmond Sunlight's database
-define('SESSION_ID', 20);
+define('SESSION_ID', 21);
 
 # Is this the main session or a special session? As defined by Richmond Sunlight's database.
 define('SESSION_SUFFIX', '');
 
 # As defined by the GA LIS' database.
-define('SESSION_LIS_ID', '171');
+define('SESSION_LIS_ID', '181');
 
 # As defined by the year.
-define('SESSION_YEAR', 2017);
+define('SESSION_YEAR', 2018);
 
 # Determine whether the GA is currently in session.
 define('IN_SESSION', 'Y');
