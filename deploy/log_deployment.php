b19bf046763553aa60dd45b97fb2377e31734fac
Waldo Jaquith <waldo@jaquith.org>
2017-11-21 22:44:49 -0500

    Log each successful deployment
    Closes #22.

---
+++ deploy/log_deployment.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once(__DIR__ . '/../includes/settings.inc.php');
+include_once(__DIR__ . '/../includes/functions.inc.php');
+include_once(__DIR__ . '/../includes/photosynthesis.inc.php');
+include_once(__DIR__ . '/../includes/vendor/autoload.php');
+
+$log = new Log;
+
+$log->put('Deploy completed', 5);
