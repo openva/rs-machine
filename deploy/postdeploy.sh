16ebde001d446ef8918d0e04fee6307ff34fd06d
Waldo Jaquith <waldo@jaquith.org>
2017-12-03 20:00:22 -0500

    Fix writeable path name

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -7,7 +7,7 @@ crontab deploy/crontab.txt
 
 # Set permissions properly, since appspec.yml gets this wrong.
 chown -R ubuntu:ubuntu /home/ubuntu/
-chmod -R g+w /vol/www/api.richmondsunlight.com/
+chmod -R g+w /home/ubuntu/
 
 # Log a record of this deployment
 echo -e "$(date)\tSuccessful deployment \r\n" >> deploy.log
