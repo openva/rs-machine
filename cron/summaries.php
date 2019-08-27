abe50baf1519c9c336b6090375cbf71b6e334344
Waldo Jaquith <waldo@jaquith.org>
2019-08-26 22:05:16 -0400

    Add failed SQL string to logging output

--- cron/summaries.php
+++ cron/summaries.php
@@ -213,7 +213,8 @@ while (($summary = fgetcsv($fp, 1000, ',')) !== FALSE)
 		$result = mysql_query($sql);
 		if (!$result)
 		{
-			$log->put('Insertion of '. strtoupper($summary['number']) . ' summary failed.', 6);
+			$log->put('Insertion of '. strtoupper($summary['number']) . ' summary failed. SQL: '
+				. $sql, 6);
 		}
 
 	}
