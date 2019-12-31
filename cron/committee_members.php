895e733f7b2f681e52a7fc42748f11d619ac0fa8
Waldo Jaquith <waldo@jaquith.org>
2019-12-31 13:04:40 -0500

    Continue work on committee-member parser
    Toward openva/richmondsunlight.com#8.

--- cron/committee_members.php
+++ cron/committee_members.php
@@ -1,18 +1,32 @@
 <?php
 
-require('includes/class.Import.php');
+include_once(__DIR__ . '/../includes/settings.inc.php');
+include_once(__DIR__ . '/../includes/functions.inc.php');
+include_once(__DIR__ . '/../includes/photosynthesis.inc.php');
+include_once(__DIR__ . '/../includes/vendor/autoload.php');
+
+$import = new Import();
 
 /*
  * Generate a list of committees as a lookup table.
  */
-$committees = create_committee_list();
+$committees = $import->create_committee_list();
 
 /*
  * Generate a list of legislators as a lookup table.
  */
-$legislators = create_legislator_list();
+$legislators = $import->create_legislator_list();
 
-if ($csv = committee_members_csv_fetch())
+if ($csv = $import->committee_members_csv_fetch())
 {
-    committee_members_csv_parse($csv, $committees, $legislators);
+    $members = $import->committee_members_csv_parse($csv, $committees, $legislators);
 }
+
+/*
+ * Compare this list to the existing member list
+ */
+$committee = new Committee();
+$committee->members();
+$legislators = $committee->members;
+
+// Store the new member list
