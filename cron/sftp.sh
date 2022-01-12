2a300d94c9063506bd783a5a0826018984947df6
Waldo Jaquith <waldo@jaquith.org>
2022-01-11 23:03:33 -0500

    Populate SFTP credentials on build
    Toward #426.

--- cron/sftp.sh
+++ cron/sftp.sh
@@ -3,7 +3,7 @@
 set filename [lindex $argv 0]
 set output_filename [lindex $argv 1]
 
-spawn sftp {USERNAME}@sftp.dlas.virginia.gov:/CSV221/csv221/$filename $output_filename
+spawn sftp {LIS_FTP_USERNAME}@sftp.dlas.virginia.gov:/CSV221/csv221/$filename $output_filename
 expect "password"
-send "{PASSWORD}\n"
+send "{LIS_FTP_PASSWORD}\n"
 interact
---
+++ deploy/sftp_credentials.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Set up FTP credentials in the SFTP script.
+sed -i -e "s|{LIS_FTP_PASSWORD}|${LIS_FTP_PASSWORD}|g" cron/sftp.sh
+sed -i -e "s|{LIS_FTP_USERNAME}|${LIS_FTP_USERNAME}|g" cron/sftp.sh
