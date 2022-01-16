3d4f1a3a4642bd07580f8d740ecc61ec74dfdefc
Waldo Jaquith <waldo@jaquith.org>
2022-01-15 22:58:46 -0500

    Enhance, update for 2022

--- cron/committee_members.php
+++ cron/committee_members.php
@@ -7,6 +7,8 @@ include_once(__DIR__ . '/../includes/vendor/autoload.php');
 
 $import = new Import();
 
+die('Read through all code before running this');
+
 /*
  * Generate a list of committees as a lookup table.
  */
@@ -17,9 +19,25 @@ $committees = $import->create_committee_list();
  */
 $legislators = $import->create_legislator_list();
 
-if ($csv = $import->committee_members_csv_fetch())
+/*
+ * Ingest the locally stored CSV file that has committee members.
+ */
+$csv = trim(file_get_contents('committee_members.csv'));
+
+/*
+ * If the CSV is at least 5,000 characters long, it's probably valid.
+ */
+if (strlen($csv) > 5000)
 {
     $new_members = $import->committee_members_csv_parse($csv, $committees, $legislators);
+}
+
+/*
+ * Make sure we have a plausible number of committee memberships.
+ */
+if (count($new_members) < 300)
+{
+    die('Error: Only ' . count($new_members) . ' entries were found in the CSV.');
 }
 
 /*
@@ -29,20 +47,20 @@ $committee = new Committee();
 $committee->members();
 $existing_members = $committee->members;
 
-exit('Make real, real sure you want to run this.');
-
 $database = new Database;
 $db = $database->connect_mysqli();
 
-// Store the new member list
+/*
+ * Store the new member list
+ */
 foreach ($new_members as $new_member)
 {
 
     $sql = 'INSERT INTO committee_members
             SET committee_id = ' . $new_member['committee_id'] . ',
             representative_id = ' . $new_member['legislator_id'] . ',
-            date_started = "2020-01-08",
+            date_started = "2022-01-12",
             date_created=now()';
-    mysqli_query($db, $sql);
+    //mysqli_query($db, $sql);
 
 }
