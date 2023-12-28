d67b2c18d7d5547be0bebcef2c77f826e5fdb1f2
Waldo Jaquith <waldo@jaquith.org>
2023-12-28 11:26:16 -0500

    Update the CSV path

--- cron/sftp.sh
+++ cron/sftp.sh
@@ -3,7 +3,7 @@
 set filename [lindex $argv 0]
 set output_filename [lindex $argv 1]
 
-spawn sftp {LIS_FTP_USERNAME}@sftp.dlas.virginia.gov:/CSV221/csv221/$filename $output_filename
+spawn sftp {LIS_FTP_USERNAME}@sftp.dlas.virginia.gov:/CSV221/csv241/$filename $output_filename
 expect "password"
 send "{LIS_FTP_PASSWORD}\n" 
 expect eof
