ae7ad2af19cc1ee904c9a967ca54ab82952473f1
Waldo Jaquith <waldo@jaquith.org>
2023-12-28 11:43:35 -0500

    Explicitly invoke expect here
    This avoids any potential problems with sftp.sh not being executable.

--- deploy/sftp_credentials.sh
+++ deploy/sftp_credentials.sh
@@ -3,3 +3,4 @@
 # Set up FTP credentials in the SFTP script.
 sed -i -e "s|{LIS_FTP_PASSWORD}|${LIS_FTP_PASSWORD}|g" cron/sftp.sh
 sed -i -e "s|{LIS_FTP_USERNAME}|${LIS_FTP_USERNAME}|g" cron/sftp.sh
+sed -i -e "s|{LIS_SESSION_ID}|${LIS_SESSION_ID}|g" cron/sftp.sh
