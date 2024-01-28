17ee026423ee852be52e31cd377422cc1bde5362
Waldo Jaquith <waldo@jaquith.org>
2024-01-27 21:39:06 -0500

    Linter

--- deploy/tests/create-shortnames.php
+++ deploy/tests/create-shortnames.php
@@ -25,46 +25,45 @@ $names[] = array(
         'casual' => 'Diggs, Danny',
         'full' => 'John Daniel "Danny" Diggs',
         'correct' => 'jddiggs');
-$names[] = array(
+    $names[] = array(
     'casual' => 'O\'Quinn, Israel',
     'full' => 'Israel D. O\'Quinn',
     'correct' => 'idoquinn');
-$names[] = array(
+    $names[] = array(
         'casual' => 'O’Quinn, Israel',
         'full' => 'Israel D. O’Quinn',
         'correct' => 'idoquinn');
-$names[] = array(
+    $names[] = array(
         'casual' => 'Keys-Gamarra, Karen',
         'full' => 'Karen K. Keys-Gamarra',
         'correct' => 'kkkeys-gamarra');
-$names[] = array(
+    $names[] = array(
         'casual' => 'Graves, Angelina',
         'full' => 'Angelia Williams Graves',
         'correct' => 'awgraves');
-$names[] = array(
+    $names[] = array(
         'casual' => 'VanValkenburg, Schuyler',
         'full' => 'Schuyler T. VanValkenburg',
         'correct' => 'stvanvalkenburg');
-$names[] = array(
+    $names[] = array(
         'casual' => 'Bennett-Parker, Elizabeth',
         'full' => 'Elizabeth B. Bennett-Parker',
         'correct' => 'ebbennett-parker');
 
-$failures = 0;
-foreach ($names as $name) {
-    $shortname = $import -> create_shortname($name['casual'], $name['full']);
-    if ($shortname != $name['correct']) {
-        echo 'FAILED: Generated ' . $shortname .' instead of ' . $name['correct'] . "\n";
-        $failures++;
+    $failures = 0;
+    foreach ($names as $name) {
+        $shortname = $import -> create_shortname($name['casual'], $name['full']);
+        if ($shortname != $name['correct']) {
+            echo 'FAILED: Generated ' . $shortname . ' instead of ' . $name['correct'] . "\n";
+            $failures++;
+        }
     }
-}
 
-echo 'Tested generating ' . count($names) . ' shortnames: ' . count($names) - $failures
+    echo 'Tested generating ' . count($names) . ' shortnames: ' . count($names) - $failures
     . ' passed, ' . $failures . ' failed.' . "\n";
 
-if ($failures > 0)
-{
-    exit(1);
-}
+    if ($failures > 0) {
+        exit(1);
+    }
 
-exit(0);
+    exit(0);
