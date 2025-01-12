f8f2c0054e2bf0e204431917aae01082e8aefa94
Waldo Jaquith <waldo@jaquith.org>
2025-01-11 22:30:49 -0500

    Handle fiscal impact statements better
    Get the URL from the CSV, and use a completely new table to store them.

--- cron/fiscal_impact.php
+++ cron/fiscal_impact.php
@@ -1,15 +1,8 @@
 <?php
 
 /*
-Sometimes there is more than one fiscal impact statement. Sometimes they reflect changes
-in the legislation, but sometimes I think they're from multiple agencies?
-
 How to tell the difference between what's an updated FIS vs. an additional one? That is,
 what replaces and what supplements?
-
-Do we store the PDF URL? Because the provided filename isn't actually the filename.
-
-Should this be in a new table?
 */
 
 // Get the CSV
@@ -28,30 +21,31 @@ $fis_data = array_map('str_getcsv', $csv);
 $fis = [];
 
 foreach ($fis_data as $statement) {
-    $bill = strtolower($statement[0]);
-    $bill = preg_replace('/([a-z]+)(0+)/', '$1', $bill);
+    $bill_number = strtolower($statement[0]);
+    $lis_id = $statement[1];
+    $pdf_url = $statement[2];
 
-    $filename = str_replace(strtoupper($bill), '', $statement[1]);
-    $filename = trim(str_replace('.PDF', '', $filename));
-
-    $fis[$bill] = $filename;
+    $fis[] = array('bill_number' => $bill_number, 'lis_id' => $lis_id, 'pdf_url' => $pdf_url);
 }
 
 // Insert the records
-foreach ($fis as $bill_number => $fis_id) {
-    $sql = 'UPDATE bills
-            SET impact_statement_id = "' . $fis_id . '"
-            WHERE number = "' . $bill_number . '" AND
-            session_id = ' . SESSION_ID;
+foreach ($fis as $statement) {
+    $sql = 'REPLACE INTO fiscal_impact_statements
+            SET
+                lis_id = "' . $statement['lis_id'] . '",
+                bill_id=(
+                    SELECT id
+                    FROM bills
+                    WHERE number = "' . $statement['bill_number'] . '"
+                    AND session_id=' . SESSION_ID . '),
+                pdf_url = "' . $statement['pdf_url'] . '",
+                date_created = NOW()';
     $result = mysqli_query($GLOBALS['db'], $sql);
     if ($result === false) {
-        $log->put('Error: Adding a fiscal impact statement ID for ' . $bill_number . ' failed: '
-            . mysqli_error($GLOBALS['db']), 4);
-        if (mysqli_error($GLOBALS['db']) == 'MySQL server has gone away') {
-            $log->put('Abandoning insertion of fiscal impact statement IDs.', 5);
-            break;
-        }
+        $log->put('Error: Adding a fiscal impact statement ID for ' . $statement['bill_number']
+            . ' failed: ' . mysqli_error($GLOBALS['db']), 4);
     } else {
-        $log->put('Added or updated a fiscal impact statement ID for ' . $bill_number . '.', 1);
+        $log->put('Added or updated a fiscal impact statement ID for ' . $statement['bill_number']
+            . '.', 2);
     }
 }
