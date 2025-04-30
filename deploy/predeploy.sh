44ab71f31b933cbb40fa086721557d78df29eeb1
Waldo Jaquith <waldo@jaquith.org>
2025-04-29 22:47:01 -0400

    Remove unnecessary alias
    This is solved with a modern Ubuntu distribution.

--- deploy/predeploy.sh
+++ deploy/predeploy.sh
@@ -17,6 +17,3 @@ sudo apt-get -y upgrade
 sudo apt-get install -y php-mysql php-memcached php-xml composer zip ruby awscli npm expect \
     memcached poppler-utils
 sudo npm i -g csvtojson
-
-# Allow "node" to invoke Node.js (as csvtojson requires).
-sudo ln -s /usr/bin/nodejs /usr/bin/node
