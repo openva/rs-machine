cbe8d05c856c91cbb2a7c8923393b642d0abfa44
Waldo Jaquith <waldo@jaquith.org>
2025-04-30 22:30:35 -0400

    Remove old PHP code

--- deploy/predeploy.sh
+++ deploy/predeploy.sh
@@ -3,12 +3,6 @@
 # Change the timezone to Eastern
 sudo timedatectl set-timezone EST
 
-# Remove all PHP packages (they may well be PHP 7)
-#sudo apt-get -y purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
-
-# Use repo for PHP 5.6.
-#sudo add-apt-repository -y ppa:ondrej/php
-
 # Update the OS
 sudo apt-get update
 sudo apt-get -y upgrade
