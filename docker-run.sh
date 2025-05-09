aea00a1d72204375f85f6f8eac7e397b8a1820ad
Waldo Jaquith <waldo@jaquith.org>
2025-05-08 21:46:36 -0400

    Load data into the database

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -e
 
+# Prepare data for the database
+
+# If the database.sql doesn't exist, create it
+if [ ! -f "deploy/database.sql" ]; then
+
+    git clone -b deploy https://github.com/openva/richmondsunlight.com.git
+
+    # Concatenate the database dumps into a single file, for MariaDB to load
+    cd richmondsunlight.com/deploy/
+    cat mysql/structure.sql mysql/basic-contents.sql mysql/test-records.sql > ../../deploy/database.sql
+    cd ..
+
+    rm -Rf richmondsunlight.com
+
+fi
+
 # Stand it up
 docker compose build && docker compose up -d
 
