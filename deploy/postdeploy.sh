a296428115f8d682e026111cd331280925df56b0
Waldo Jaquith <waldo@jaquith.org>
2025-10-28 23:02:41 -0400

    Set file ownership properly on deployment
    Move this into appspec.yml.

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -5,9 +5,5 @@ cd /home/ubuntu/rs-machine/ || exit
 # Set up the crontab
 crontab deploy/crontab.txt
 
-# Set permissions properly, since appspec.yml gets this wrong.
-chown -R ubuntu:ubuntu /home/ubuntu/
-chmod -R g+w /home/ubuntu/
-
 # Log a record of this deployment
 echo -e "$(date)\tSuccessful deployment \r" >> deploy.log
