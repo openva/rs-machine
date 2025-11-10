c82c326f539f763a5aaca8e092bb0637bf1551ce
Waldo Jaquith <waldo@jaquith.org>
2025-11-09 20:31:19 -0500

    Allow for locally-defined constants
    This is to support storing the Open AI and LIS API keys, to use in Docker.

--- deploy/settings-docker.inc.php
+++ deploy/settings-docker.inc.php
@@ -8,83 +8,100 @@
 #
 ###
 
+if (!function_exists('rs_define')) {
+    /**
+     * Define a constant unless it was already provided (e.g., via local overrides).
+     */
+    function rs_define(string $name, $value): void
+    {
+        if (!defined($name)) {
+            define($name, $value);
+        }
+    }
+}
+
+$rs_local_settings_file = __DIR__ . '/settings.local.inc.php';
+if (is_readable($rs_local_settings_file)) {
+    require $rs_local_settings_file;
+}
+
 # THE CURRENT SESSION
 # As defined by Richmond Sunlight's database
-define('SESSION_ID', 30);
+rs_define('SESSION_ID', 30);
 
 # Is this the main session or a special session? As defined by Richmond Sunlight's database.
-define('SESSION_SUFFIX', '');
+rs_define('SESSION_SUFFIX', '');
 
 # As defined by the GA LIS' database.
-define('SESSION_LIS_ID', '241');
+rs_define('SESSION_LIS_ID', '251');
 
 # As defined by the year.
-define('SESSION_YEAR', 2024);
+rs_define('SESSION_YEAR', 2025);
 
 # Start and end of this session.
-define('SESSION_START', '2024-01-10');
-define('SESSION_END', '2024-03-09');
+rs_define('SESSION_START', '2025-01-08');
+rs_define('SESSION_END', '2025-12-31');
 
 # Set the FTP auth pair for legislative data.
-define('LIS_FTP_USERNAME', '');
-define('LIS_FTP_PASSWORD', '');
+rs_define('LIS_FTP_USERNAME', '');
+rs_define('LIS_FTP_PASSWORD', '');
 
 # The DSN to connect to MySQL.
-define('PDO_DSN', 'mysql:host=db;dbname=richmondsunlight');
-define('PDO_SERVER', 'db');
-define('PDO_USERNAME', 'ricsun');
-define('PDO_PASSWORD', 'password');
-define('MYSQL_DATABASE', 'richmondsunlight');
+rs_define('PDO_DSN', 'mysql:host=db;dbname=richmondsunlight');
+rs_define('PDO_SERVER', 'db');
+rs_define('PDO_USERNAME', 'ricsun');
+rs_define('PDO_PASSWORD', 'password');
+rs_define('MYSQL_DATABASE', 'richmondsunlight');
 
 # The API URL.
-define('API_URL', 'http://api/');
+rs_define('API_URL', 'http://api/');
 
 # Specify how to connect to Memcached.
-define('MEMCACHED_SERVER', 'localhost');
-define('MEMCACHED_PORT', '11211');
+rs_define('MEMCACHED_SERVER', 'localhost');
+rs_define('MEMCACHED_PORT', '11211');
 
 # The House Speaker's IDs. This is used in update_vote.php to translate votes credited to
 # "H0000," which bizarrely indicates the speaker, to that legislator's ID, and in
 # Video::identify_speakers to match the speaker to her identity. Here, H322 and 455 indicate
 # Don Scott.
-define('HOUSE_SPEAKER_LIS_ID', 'H322');
-define('HOUSE_SPEAKER_ID', '455');
+rs_define('HOUSE_SPEAKER_LIS_ID', 'H322');
+rs_define('HOUSE_SPEAKER_ID', '455');
 
 # Set the directory to look to for cache data.
-define('CACHE_DIR', '/vol/www/richmondsunlight.com/html/cache/');
+rs_define('CACHE_DIR', '/vol/www/richmondsunlight.com/html/cache/');
 
 # ESTABLISH API KEYS
 
 # Google Maps
-define('GMAPS_KEY', '');
+rs_define('GMAPS_KEY', '');
 
 # Open States (Sunlight Foundation) API Key
-define('OPENSTATES_KEY', '');
+rs_define('OPENSTATES_KEY', '');
 
 # Open Virginia / Virginia Decoded API Key
 # (We're inconsistent in our nomenclature.)
-define('OPENVA_KEY', '');
-define('VA_DECODED_KEY', '');
+rs_define('OPENVA_KEY', '');
+rs_define('VA_DECODED_KEY', '');
 
 # Mapbox API access token
-define('MAPBOX_TOKEN', '');
+rs_define('MAPBOX_TOKEN', '');
 
 # LIS API token
-define('LIS_KEY', '');
+rs_define('LIS_KEY', '');
 
 # Logging verbosity, on a scale of 1–8
-define('LOG_VERBOSITY', 3);
+rs_define('LOG_VERBOSITY', 3);
 
 # Slack API URL
-define('SLACK_WEBHOOK', '');
+rs_define('SLACK_WEBHOOK', '');
 
 # OpenAI API key
-define('OPENAI_KEY', '');
+rs_define('OPENAI_KEY', '');
 
 # AWS auth info
 # This is only used in some RS instances.
-define('AWS_ACCESS_KEY', '');
-define('AWS_SECRET_KEY', '');
+rs_define('AWS_ACCESS_KEY', '');
+rs_define('AWS_SECRET_KEY', '');
 
 # The list of words that, when used, will lead to instant blacklisting. They're rot 13ed here.
 $GLOBALS['banned_words'] = array('fuvg','shpx','nffubyr','chffl','phag','shpxre','zbgureshpxre',
@@ -107,12 +124,12 @@ if (
     &&
     time() <= strtotime(SESSION_END)
 ) {
-    define('IN_SESSION', true);
+    rs_define('IN_SESSION', true);
 } else {
-    define('IN_SESSION', true);
+    rs_define('IN_SESSION', true);
 }
 if (date('n') >= 11 || date('n') <= 4) {
-    define('LEGISLATIVE_SEASON', true);
+    rs_define('LEGISLATIVE_SEASON', true);
 } else {
-    define('LEGISLATIVE_SEASON', true);
+    rs_define('LEGISLATIVE_SEASON', true);
 }
