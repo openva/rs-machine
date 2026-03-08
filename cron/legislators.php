652b15ebe91c9e9aaccd5302f92d324512ae2d4b
Waldo Jaquith <waldo@jaquith.org>
2026-03-07 23:43:41 -0500

    Add additional duplicate-checking test

--- cron/legislators.php
+++ cron/legislators.php
@@ -188,7 +188,7 @@ foreach ($house_members as $member) {
     if (empty($member['lis_id'])) {
         continue;
     }
-    $lis_id = 'H' . str_pad($member['lis_id'], 4, '0', STR_PAD_LEFT);
+    $lis_id = build_legacy_lis_id('house', $member['lis_id']);
     $delegates[$lis_id] = $member['name_formal'] ?? $member['name'] ?? $member['name_formatted'] ?? $lis_id;
 }
 
